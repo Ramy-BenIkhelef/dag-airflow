3178d9396c40c4eebe72c7c40b53a7d1563a005f
Ben Ikhelef Ramy <85173815+Ramy-BenIkhelef@users.noreply.github.com>
2026-07-23 12:55:21 +0000

    test taskgroup

--- dag_formation.py
+++ dag_formation.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.trigger_rule import TriggerRule
+from airflow.utils.task_group import TaskGroup
  
 with DAG(
     dag_id="dag_formation",
@@ -10,23 +11,32 @@ with DAG(
     catchup=False,
     tags=["guide"],
 ) as dag:
-    hello = BashOperator(
-        task_id="hello",
-        bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date",
-        trigger_rule=TriggerRule.ONE_FAILED
-    )
-    ex_1 = BashOperator(
-        task_id="ex_1",
-        bash_command="sleep 10"
-    )   
-    ex_2 = BashOperator(
-        task_id="ex_2",
-        bash_command="echo 'troisieme tache'"
-    )
 
-    ex_4 = BashOperator(
-        task_id="ex_4",
-        bash_command="exit 1"
-    )
+    with TaskGroup(group_id="groupe_1") as groupe_1:
 
-ex_1 >> ex_4 >> [hello , ex_2]
+        hello = BashOperator(
+            task_id="hello",
+            bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date",
+            # trigger_rule=TriggerRule.ONE_FAILED
+        )
+        ex_1 = BashOperator(
+            task_id="ex_1",
+            bash_command="sleep 10"
+        )
+
+        [hello , ex_1]
+
+    with TaskGroup(group_id="groupe_2") as groupe_2:  
+        ex_2 = BashOperator(
+            task_id="ex_2",
+            bash_command="echo 'troisieme tache'"
+        )
+
+        ex_4 = BashOperator(
+            task_id="ex_4",
+            bash_command="exit 1"
+        )
+        [ex_2 , ex_4]
+
+groupe_1 >> groupe_2 
+
