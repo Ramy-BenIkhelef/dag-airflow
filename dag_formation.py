beccdf1484b9afa747f3b25f4ad82f3cf6238e25
Ben Ikhelef Ramy <85173815+Ramy-BenIkhelef@users.noreply.github.com>
2026-07-23 14:00:48 +0000

    ex_xcom

--- dag_formation.py
+++ dag_formation.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.utils.trigger_rule import TriggerRule
-from airflow.utils.task_group import TaskGroup
 from airflow.models import Variable
- 
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.utils.task_group import TaskGroup
 
-test_variable = Variable.get("key_1")
+def fonction_ex_1():
+    test_variable = Variable.get("key_1")
+    ti.xcom_push(key='ex_1', value="ma clé xcom")
+
+def fonction_ex_2(ti):
+    valeur = ti.xcom_pull(task_ids="groupe_1.ex_1")
+    print(f"Valeur reçue depuis XCom : {valeur}")
 
 with DAG(
     dag_id="dag_formation",
@@ -18,30 +22,17 @@ with DAG(
 
     with TaskGroup(group_id="groupe_1") as groupe_1:
 
-        hello = BashOperator(
-            task_id="hello",
-            bash_command="echo 'Airflow est prêt sur cette EC2'; hostname; date",
-            # trigger_rule=TriggerRule.ONE_FAILED
-        )
-        ex_1 = BashOperator(
+        ex_1 = PythonOperator(
             task_id="ex_1",
-            bash_command=f"echo '{test_variable}'",
-            do_xcom_push=True
+            python_callable=fonction_ex_1
         )
 
-        [hello , ex_1]
+    with TaskGroup(group_id="groupe_2") as groupe_2:
 
-    with TaskGroup(group_id="groupe_2") as groupe_2:  
-        ex_2 = BashOperator(
+        ex_2 = PythonOperator(
             task_id="ex_2",
-            bash_command="echo 'troisieme tache'"
+            python_callable=fonction_ex_2
         )
 
-        ex_4 = BashOperator(
-            task_id="ex_4",
-            bash_command="exit 1"
-        )
-        [ex_2 , ex_4]
-
-groupe_1 >> groupe_2 
+    groupe_1 >> groupe_2
 
