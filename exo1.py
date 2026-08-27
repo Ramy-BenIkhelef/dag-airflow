8f481b3c914d0dea5e2ef231eebb85161663003b
Ben Ikhelef Ramy <85173815+Ramy-BenIkhelef@users.noreply.github.com>
2026-08-27 12:31:26 +0000

    test xcom

--- exo1.py
+++ exo1.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+
 with DAG(
     dag_id="hello_airflow",
     start_date=datetime(2026, 8, 27),
@@ -13,3 +14,12 @@ with DAG(
         task_id="tache1",
         bash_command="echo 'Bonjour depuis Airflow'",
     )
+
+    tache2 = BashOperator(
+        task_id="tache2",
+        bash_command="""
+            echo "Valeur Tache1 depuis XCom : {{ ti.xcom_pull(task_ids='tache1') }}
+        """,
+    ) 
+
+    tache1 >> tache2   
