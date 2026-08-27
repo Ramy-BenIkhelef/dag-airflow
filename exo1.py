720a0a0083a6440d0543fe03d197756c9a2f58bd
Ben Ikhelef Ramy <85173815+Ramy-BenIkhelef@users.noreply.github.com>
2026-08-27 12:47:06 +0000

    schedule monthly

--- exo1.py
+++ exo1.py
@@ -6,7 +6,7 @@ from datetime import datetime
 with DAG(
     dag_id="hello_airflow",
     start_date=datetime(2026, 8, 27),
-    schedule=None,
+    schedule=@monthly,
     catchup=False,
     tags=["exo1"],
 ) as dag: 
