76dbcc86056a2842ab0899f7e57290961ef7cd6a
Ben Ikhelef Ramy <85173815+Ramy-BenIkhelef@users.noreply.github.com>
2026-08-27 12:11:22 +0000

    push exo1

---
+++ exo1.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="hello_airflow",
+    start_date=datetime(2026, 8, 27),
+    schedule=None,
+    catchup=False,
+    tags=["exo1"],
+) as dag: 
+    tache1 = BashOperator(
+        task_id="tache1",
+        bash_command="echo 'Bonjour depuis Airflow'",
+    )
